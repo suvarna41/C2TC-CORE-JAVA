27cf6e5b279355659386bd191bebdb8b6898862d
suvarna41 <65711403+suvarna41@users.noreply.github.com>
2022-03-10 19:51:07 +0530

    first1 commit

---
+++ Day11/src/day11/Demo.java
@@ -0,0 +1,10 @@
+package day11;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Hello!! Welcome to the C2TC Training");
+	}
+
+}
