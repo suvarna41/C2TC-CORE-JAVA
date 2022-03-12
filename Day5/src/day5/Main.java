fb570adcc0257e448fef883b4b4a7d6425d91c5d
suvarna41 <65711403+suvarna41@users.noreply.github.com>
2022-03-12 20:11:37 +0530

    Fifth Commit

---
+++ Day5/src/day5/Main.java
@@ -0,0 +1,15 @@
+package day5;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        String firstName = "Suvarna";
+        String lastName = "Kurdekar";
+        String fullName = firstName +" "+ lastName;
+        System.out.println(fullName);
+        System.out.println(fullName.toUpperCase());
+        System.out.println(fullName.toLowerCase());
+	}
+
+}
