c78079807c77c9cf840a1201268a9c65b69c813a
suvarna41 <65711403+suvarna41@users.noreply.github.com>
2022-03-14 17:40:15 +0530

    Sixth Commit

---
+++ Day6/src/day6/Main.java
@@ -0,0 +1,16 @@
+package day6;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int time = 20;
+        if(time < 18) {
+        	System.out.println("Good day.");
+        }
+        else {
+        	System.out.println("Good Evening.");
+        }
+	}
+
+}
