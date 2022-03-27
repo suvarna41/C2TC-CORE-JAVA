1559a03cf17f32a1bcafb6debb23527b2d12940a
suvarna41 <65711403+suvarna41@users.noreply.github.com>
2022-03-27 20:39:42 +0530

    Thirteenth Commit

---
+++ Simple.java
@@ -0,0 +1,10 @@
+package com.applet;
+
+import java.awt.*;
+import java.applet.*;
+
+public class Simple extends Applet {
+      public void paint(Graphics g) {
+    	  g.drawString("A simple Applet", 20, 20);
+      }
+}
